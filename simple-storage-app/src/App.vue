433307641cfc2c5f48bdd4efcf8cf9828dfaaef3
Franz Bendezu <franzant20@gmail.com>
2023-08-26 17:51:57 -0500

    feat: config ether

--- simple-storage-app/src/App.vue
+++ simple-storage-app/src/App.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const count = ref(0)
+import { onMounted } from 'vue'
+import { ethers } from 'ethers'
+import type { Signer, AbstractProvider } from 'ethers'
 
-const increment = ()=>{
-  count.value = count.value + 1
+let signer: Signer | null = null
+let provider: AbstractProvider | null = null
+
+async function initEthConfig() {
+  const _window = window as any
+  if (_window.ethereum == null) {
+    provider = ethers.getDefaultProvider('sepolia', {
+      infura: 'c5f492cc1f1e481cbe3a8ce88bcaf396'
+    })
+  } else {
+    const browserProvider = new ethers.BrowserProvider(_window.ethereum)
+    provider = browserProvider
+    signer = await browserProvider.getSigner()
+  }
 }
 
+onMounted(async () => {
+  await initEthConfig()
+})
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <button v-on:click="increment()" >
-      Incrementar {{ count }}
-    </button>
   </header>
 </template>
 
@@ -28,55 +41,4 @@ header {
   margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
