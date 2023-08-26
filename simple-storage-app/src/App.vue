82f1c548b3ce66c4288050638cbb5330d86c678c
Franz Bendezu <franzant20@gmail.com>
2023-08-26 18:35:20 -0500

    feat: read and update data in contract

--- simple-storage-app/src/App.vue
+++ simple-storage-app/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { ethers } from 'ethers'
 import type { Signer, AbstractProvider } from 'ethers'
+import SimpleStorage from '@/assets/SimpleStorage.json'
 
 let signer: Signer | null = null
 let provider: AbstractProvider | null = null
@@ -22,12 +23,72 @@ async function initEthConfig() {
 onMounted(async () => {
   await initEthConfig()
 })
+
+const contractAddress = '0x4F2Aad93c8B56BD89CBB55a88F87f377deb2ab25'
+
+const getNumber = ref('')
+async function numberGet() {
+  try {
+    const contract = new ethers.Contract(contractAddress, SimpleStorage.abi, provider)
+    const result = await contract.get()
+    getNumber.value = result.toString()
+  } catch (error) {
+    console.error('Error al obtener el número:', error)
+  }
+}
+
+const newData = ref(0)
+async function numberSet() {
+  console.log(newData.value)
+  try {
+    const contract = new ethers.Contract(contractAddress, SimpleStorage.abi, signer)
+    const transaction = await contract.set(newData.value)
+    await transaction.wait()
+    // Update state or show success message
+  } catch (error) {
+    console.error('Error setting number:', error)
+  }
+}
+
+/* function handleChange(event: Event){
+  const target = event.target as HTMLInputElement
+  newData.value = target.value
+}
+*/
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
   </header>
+  <div class="card">
+    <h2 class="subtitle">Obtener tu uint256</h2>
+    <form class="form" @submit.prevent="numberGet">
+      <button class="button" type="submit">Obtener</button>
+      <label>
+        Tu uint256:
+        <input class="input" type="text" :value="getNumber" readonly />
+      </label>
+    </form>
+
+    <h2 class="subtitle">Establecer tu uint256</h2>
+    <form class="form" @submit.prevent="numberSet" >
+      <label>
+        Establece tu uint256: 
+        <!-- {{ newData  }} -->
+        <!-- 
+          :value
+          @input
+         -->
+         <!-- 
+          <input class="input" type="text" v-bind:value="newData" v-on:input="handleChange($event)" />
+          <input class="input" type="text" :value="newData" @input="handleChange($event)" />
+          -->
+        <input class="input" type="text" v-model.number="newData" />
+      </label>
+      <button class="button" type="submit">Confirmar</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
@@ -41,4 +102,29 @@ header {
   margin: 0 auto 2rem;
 }
 
+.card {
+  max-width: 500px;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.form {
+  height: 20vh;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+}
+
+.button {
+  background-color: #00d1b2;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  width: 100%;
+}
 </style>
